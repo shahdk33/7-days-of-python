22d9f0fd3f44f544ff404aab519206e66e008f43
Shahd Khartabil <sh977589@dal.ca>
2023-05-26 14:22:05 -0300

    game board

--- day2/main.py
+++ day2/main.py
@@ -12,3 +12,22 @@ SNAKE_COLOR = '#58AB9A'
 FOOD_COLOR = '#587BAB'
 BACK_COLOR = '#38555D'
 
+
+#making the gui window
+root = Tk()
+
+#title
+root.title("Snake Game")
+root.resizable(False,False)
+
+score = 0
+direction = 'down'
+
+#score label
+label = Label(root, text="Score:{}".format(score), font=('Arial', 30))
+label.pack()
+
+canvas = Canvas(root, bg = BACK_COLOR, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+root.mainloop()
