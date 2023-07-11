90af14abca4288c134ad7d501c962d49d05001c8
Shahd Khartabil <sh977589@dal.ca>
2023-07-11 20:58:36 -0300

    web scraper initialcode

---
+++ day7/web_scraper.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+
+web_page = requests.get("https://weather.gc.ca/canada_e.html")
+doc = BeautifulSoup(web_page.text, "html.parser")
+
+
+cities = ['Calgary', 'Halifax', 'Montréal', 'Toronto', 'Vancouver']
+
+#open file
+with open('weather.txt', 'w') as f:
+
+    #get all things in table
+    for row in doc.find_all('tbody')[0].find_all('tr'):
+
+        #get all content thats table data
+        cells = row.find_all('td')
+
+        #get city name
+        city = cells[0].get_text().strip()
+        #check if the city is from the ones we want
+        if city in cities:
+            #Then fetch the temp (in cell 2)
+            temperature = cells[2].get_text().strip()
+
+            #write to the file
+
+            f.write(f"{city}: {temperature}\n")
+    
+
