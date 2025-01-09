31bf0defe608fc50d7fcc0040c4635e26cabf4c6
shahdk33 <sh977589@dal.ca>
2025-01-09 18:35:05 -0400

    env change

--- day6/frontend/frontend/src/components/recipeApp.js
+++ day6/frontend/frontend/src/components/recipeApp.js
@@ -19,7 +19,7 @@ function RecipeApp() {
         setLoading(true); // Start loading
 
         try {
-            const response = await fetch('/recipes', {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/recipes`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ ingredients: ingredients.split(',') }),
